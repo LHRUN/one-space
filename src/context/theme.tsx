969ef97e99011f4faed63754dd4d001b327b8d15
LH_R <song.lhlh@gmail.com>
2024-09-29 18:21:01 +0800

    fix: initialise theme mode error

--- src/context/theme.tsx
+++ src/context/theme.tsx
@@ -33,18 +33,16 @@ export function ThemeProvider({
   useEffect(() => {
     const root = window.document.documentElement
     root.classList.remove("light", "dark")
+    let newTheme = theme
     if (theme === "system") {
-      const systemTheme = window.matchMedia("(prefers-color-scheme: dark)")
+      newTheme = window.matchMedia("(prefers-color-scheme: dark)")
         .matches
         ? "dark"
         : "light"
-
-      root.classList.add(systemTheme)
-      return
     }
 
-    root.classList.add(theme)
-    setTheme(theme)
+    root.classList.add(newTheme)
+    setTheme(newTheme)
   }, [theme])
 
   const value = {
